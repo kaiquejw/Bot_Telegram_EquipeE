8d2162d86b0c0ef6e0512838dcfadd3cf3e58c37
kaiquejw <kaiquecapt@gmail.com>
2026-08-13 20:34:51 -0300

    PROD VIVIANEBATOREE

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ TZ = ZoneInfo("America/Sao_Paulo")
 
 
 HORA_ALVO = 20
-MINUTO_ALVO = 30
+MINUTO_ALVO = 45
 SEGUNDO_ALVO = 0
 
 ANTECIPACAO_S = 0.0
@@ -33,21 +33,14 @@ DESISTIR_APOS_S = 120
 
 CONTAS = [
 
-        #  20h30 Senha Grupo Normal -1003927816412
-    {
-        "nome": "Priscila",
-        "secret_name": "SESSION_PRISCILA",
-        "chat_id": -1003927816412,
-        "msg": "Priscila x Andre raio 4"
-    },
 
         #  20h45 Senha Grupo Preferencial -1003552682244  
-    #{
-    #    "nome": "VivianeBatore",
-    #    "secret_name": "SESSION_VIVIANEBATORE",
-    #    "chat_id": -1003552682244,
-    #    "msg": "Viviane x batoré raio 2"
-    #},
+    {
+        "nome": "VivianeBatore",
+        "secret_name": "SESSION_VIVIANEBATORE",
+        "chat_id": -1003552682244,
+        "msg": "Viviane x batoré raio 2"
+    },
 
 ]
 
