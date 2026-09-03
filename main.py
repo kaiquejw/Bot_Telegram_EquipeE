6fffca2ea2a5517a2da3e5078529b1c3a1f02cb5
kaiquejw <kaiquecapt@gmail.com>
2026-09-03 17:01:54 -0300

    ULTIMO TESTEE

--- main.py
+++ main.py
@@ -21,24 +21,72 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 TZ = ZoneInfo("America/Sao_Paulo")  
 
 
-HORA_ALVO = 20
-MINUTO_ALVO = 45
+HORA_ALVO = 17
+MINUTO_ALVO = 10
 SEGUNDO_ALVO = 0
 
 ANTECIPACAO_S = 0.0
-LAUNCH_INTERVAL = 0.030
+LAUNCH_INTERVAL = 0.035
 DESISTIR_APOS_S = 280
 
 
 
 CONTAS = [
 
+        #  19h00 P3 Anexo Normal -1002912888763
+    {
+        "nome": "Giovana",
+        "secret_name": "SESSION_GIOVANA",
+        "chat_id": -5299568827,
+         "msg": "Bruna x João Victor 4x2 \n Giovana x João Victor 4x2"
+    },
+
+        #  20h00 GRUPO NORMAL. -1004339638597
+    {
+        "nome": "Luciane",
+        "secret_name": "SESSION_LUCIANE",
+        "chat_id": -1004368969674,
+        "msg": "Luciane x Felipe R 4 c 3"
+    },
+
+        #  20h00 GRUPO NORMAL. -1004339638597
+    {
+        "nome": "Gabyzinha",
+        "secret_name": "SESSION_GABYZINHA",
+        "chat_id": -5001038132,
+        "msg": "Gaby x Paraguaio \n Raio7x7dobra"
+    },
+
+        #  20h30 Senha Grupo Normal -1003927816412
+    {
+        "nome": "Luciana",
+        "secret_name": "SESSION_LUCIANA",
+        "chat_id": -5263208911,
+        "msg": "Luciana x Primavera R2"
+    },
+
+        #  20h30 Senha Grupo Normal -1003927816412
+    {
+        "nome": "Carol",
+        "secret_name": "SESSION_CAROL",
+        "chat_id": -5336863745,
+        "msg": "Carol x Lucas r3"
+    },
+
+        #  20h30 Senha Grupo Normal -1003927816412
+    {
+        "nome": "Tatiane",
+        "secret_name": "SESSION_TATIANE",
+        "chat_id": -5117621304,
+        "msg": "Tatiane x Adilson raio 5"
+    },
+
         #  20h45 Senha Grupo Preferencial -1003552682244
     {
-        "nome": "Katia",
-        "secret_name": "SESSION_KATIA",
-        "chat_id": -1003552682244,
-        "msg": "Katia pantanal r2 laudo"
+        "nome": "Tamires",
+        "secret_name": "SESSION_TAMIRES",
+        "chat_id": -5391035776,
+        "msg": "Tamires x Rodrigo R 3 (gestante)"
     },
 
 ]
